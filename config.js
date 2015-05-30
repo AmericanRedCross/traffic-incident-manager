319dd2bc3761f170b6c564a2f5ee212eafb78818
Kevin Lustig <kevin@thejourneylab.com>
2015-05-30 23:45:17 +0000

    Complete project commit, ready to deploy

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+	siteName: "Nepal Asset Manager",
+	description: "Use this site to manage and maintain map assets for the Nepal relief response.",
+	db: "arc_assets",
+	asset_opts: {
+		extents: [
+			"Arghakhanchi",	"Bagmati",	"Bara",	"Bhaktapur",	"Chitawan",	"Dhading",	"Dhanusa",	"Dhaualagiri",	"Dolakha",	"Gandaki",	"Ghorkha",	"Gulmi",	"Janakpur",	"Kapilbastu",	"Kaski",	"Kathmandu",	"Kavrepalanchok",	"Koshi",	"Lalitpur",	"Lamjung",	"Lumbini",	"Mahottari",	"Makwanpur",	"Manang",	"Mechi",	"Narayani",	"Nawalparasi",	"Nepal",	"Nuwakot",	"Okhaldhunga",	"Palpa",	"Parsa",	"Ramechhap",	"Rapti",	"Rasuwa",	"Rautahat",	"Rupandehi",	"Sagarmatha",	"Sarlahi",	"Sindhuli",	"Sindhupalchok",	"Syangja",	"Tanahu",	"World"
+		],
+		sectors: [
+			"3W",
+			"Accessibility",
+			"Admin-boundaries",
+			"Affected",
+			"Allocation",
+			"AmericanRedCross",
+			"Atlas-map",
+			"Baseline",
+			"Coordination",
+			"Damage",
+			"Distributions",
+			"Earthquake",
+			"Health",
+			"Hillshade",
+			"Hubs",
+			"IDP-Camps",
+			"IRA",
+			"Logistics",
+			"NRCS",
+			"Population",
+			"Priority",
+			"RedCrossRedCrescent",
+			"Roads",
+			"Shelter",
+			"VDC",
+			"Web-map"
+		]
+	}
+}
