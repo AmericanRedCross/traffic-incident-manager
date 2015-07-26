01c3bb665efde82b81e7d71385b0ceb0bc3d4130
Kevin Lustig <kevin@thejourneylab.com>
2015-07-26 04:35:03 +0400

    Adding map handling for centroid coordinates, addresses #20

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ module.exports = {
 	description: "Use this site to manage and maintain map assets for relief responses.",
 	db: "arc_assets",
 	port: 8888,
+	mapboxId: "kevinlustig.n0dmkkj4",
+	mapboxToken: "pk.eyJ1Ijoia2V2aW5sdXN0aWciLCJhIjoiNDgzNDhhNzRhMzRhN2FlOGEzZDkzOWNkMDcyMDIzMzIifQ.2fQQqIf5HJsjHjMPsvNTOQ",
 	asset_opts: {
 		extents: [
 			"Arghakhanchi",	"Bagmati",	"Bara",	"Bhaktapur",	"Chitawan",	"Dhading",	"Dhanusa",	"Dhaualagiri",	"Dolakha",	"Gandaki",	"Ghorkha",	"Gulmi",	"Janakpur",	"Kapilbastu",	"Kaski",	"Kathmandu",	"Kavrepalanchok",	"Koshi",	"Lalitpur",	"Lamjung",	"Lumbini",	"Mahottari",	"Makwanpur",	"Manang",	"Mechi",	"Narayani",	"Nawalparasi",	"Nepal",	"Nuwakot",	"Okhaldhunga",	"Palpa",	"Parsa",	"Ramechhap",	"Rapti",	"Rasuwa",	"Rautahat",	"Rupandehi",	"Sagarmatha",	"Sarlahi",	"Sindhuli",	"Sindhupalchok",	"Syangja",	"Tanahu",	"World"
