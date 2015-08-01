6b3452580498dc3c7a8ac29f7595548b96867dc3
Kevin Lustig <kevin@thejourneylab.com>
2015-08-01 06:46:31 +0400

    Added customization of tags, geolocation, and document types to config.js, updated back and front ends to handle customized tags, fixed bug with geolocation map, closes #24, closes #25

--- config.js
+++ config.js
@@ -6,36 +6,58 @@ module.exports = {
 	mapboxId: "kevinlustig.n0dmkkj4",
 	mapboxToken: "pk.eyJ1Ijoia2V2aW5sdXN0aWciLCJhIjoiNDgzNDhhNzRhMzRhN2FlOGEzZDkzOWNkMDcyMDIzMzIifQ.2fQQqIf5HJsjHjMPsvNTOQ",
 	asset_opts: {
-		extents: [
-			"Arghakhanchi",	"Bagmati",	"Bara",	"Bhaktapur",	"Chitawan",	"Dhading",	"Dhanusa",	"Dhaualagiri",	"Dolakha",	"Gandaki",	"Ghorkha",	"Gulmi",	"Janakpur",	"Kapilbastu",	"Kaski",	"Kathmandu",	"Kavrepalanchok",	"Koshi",	"Lalitpur",	"Lamjung",	"Lumbini",	"Mahottari",	"Makwanpur",	"Manang",	"Mechi",	"Narayani",	"Nawalparasi",	"Nepal",	"Nuwakot",	"Okhaldhunga",	"Palpa",	"Parsa",	"Ramechhap",	"Rapti",	"Rasuwa",	"Rautahat",	"Rupandehi",	"Sagarmatha",	"Sarlahi",	"Sindhuli",	"Sindhupalchok",	"Syangja",	"Tanahu",	"World"
+		geolocation: true,
+		types: [
+			"Map",
+			"Web Map",
+			"Infographic",
+			"Situation Report"
 		],
-		sectors: [
-			"3W",
-			"Accessibility",
-			"Admin-boundaries",
-			"Affected",
-			"Allocation",
-			"AmericanRedCross",
-			"Atlas-map",
-			"Baseline",
-			"Coordination",
-			"Damage",
-			"Distributions",
-			"Earthquake",
-			"Health",
-			"Hillshade",
-			"Hubs",
-			"IDP-Camps",
-			"IRA",
-			"Logistics",
-			"NRCS",
-			"Population",
-			"Priority",
-			"RedCrossRedCrescent",
-			"Roads",
-			"Shelter",
-			"VDC",
-			"Web-map"
-		]
+		tags: {
+			"Extent": {
+				required: false,
+				values: [
+					"Arghakhanchi",	"Bagmati",	"Bara",	"Bhaktapur",	"Chitawan",	"Dhading",	"Dhanusa",	"Dhaualagiri",	"Dolakha",	"Gandaki",	"Ghorkha",	"Gulmi",	"Janakpur",	"Kapilbastu",	"Kaski",	"Kathmandu",	"Kavrepalanchok",	"Koshi",	"Lalitpur",	"Lamjung",	"Lumbini",	"Mahottari",	"Makwanpur",	"Manang",	"Mechi",	"Narayani",	"Nawalparasi",	"Nepal",	"Nuwakot",	"Okhaldhunga",	"Palpa",	"Parsa",	"Ramechhap",	"Rapti",	"Rasuwa",	"Rautahat",	"Rupandehi",	"Sagarmatha",	"Sarlahi",	"Sindhuli",	"Sindhupalchok",	"Syangja",	"Tanahu",	"World"
+				]
+			},
+			"Sector": {
+				required: true,
+				values: [
+					"3W",
+					"Accessibility",
+					"Admin-boundaries",
+					"Affected",
+					"Allocation",
+					"AmericanRedCross",
+					"Atlas-map",
+					"Baseline",
+					"Coordination",
+					"Damage",
+					"Distributions",
+					"Earthquake",
+					"Health",
+					"Hillshade",
+					"Hubs",
+					"IDP-Camps",
+					"IRA",
+					"Logistics",
+					"NRCS",
+					"Population",
+					"Priority",
+					"RedCrossRedCrescent",
+					"Roads",
+					"Shelter",
+					"VDC",
+					"Web-map"
+				]
+			},
+			"Other": {
+				required: false,
+				values: [
+					"Testing 1",
+					"Testing 2"
+				]
+			}
+		}
 	}
 }
